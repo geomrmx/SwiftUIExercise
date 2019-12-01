da3ae5a3908f5646cbdcdb3a23198a9d11bde2df
Guillermo Olmedo <geomormx@gmail.com>
2019-12-01 09:29:20 -0600

    UI controls, user profile

---
+++ Landmarks/Landmarks/Supporting Views/HikeBadge.swift
@@ -0,0 +1,31 @@
+//
+//  HikeBadge.swift
+//  Landmarks
+//
+//  Created by Blue Star on 12/1/19.
+//  Copyright © 2019 Guillermo Olmedo. All rights reserved.
+//
+
+import SwiftUI
+
+struct HikeBadge: View {
+    var name: String
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            Badge()
+                .frame(width: 300.0, height: 300.0)
+                .scaleEffect(1.0 / 3.0)
+                .frame(width: 100, height: 100)
+            Text(name)
+                .font(.caption)
+                .accessibility(label: Text("Badge for \(name)."))
+        }
+    }
+}
+
+struct HikeBadge_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeBadge(name: "Preview Testing")
+    }
+}
